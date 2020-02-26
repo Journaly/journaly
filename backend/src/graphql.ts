6ec2fcecaf6f3b97bc8b45e27c22061e74dffec4
Robin MacPherson <robin@kumabrand.com>
2020-02-26 07:56:01 -0800

    Write first queries and mutations

--- backend/src/graphql.ts
+++ backend/src/graphql.ts
@@ -16,3 +16,64 @@ schema.objectType({
     t.model.email()
   },
 })
+
+schema.queryType({
+  definition(t) {
+    t.list.field('allPosts', {
+      type: 'Post',
+      resolve: async (_parent, _args, ctx) => {
+        return ctx.db.post.findMany()
+      },
+    })
+
+    // t.field('hello', {
+    //   type: 'World',
+    //   args: {
+    //     world: schema.stringArg({ required: false }),
+    //   },
+    //   async resolve(_root, args, ctx) {
+    //     const worldToFindByName = args.world ?? 'Earth'
+    //     const world = await ctx.db.world.findOne({
+    //       where: {
+    //         name: worldToFindByName,
+    //       },
+    //     })
+    //     if (!world) throw new Error(`No such world named "${args.world}"`)
+    //     return world
+    //   },
+    // })
+    // t.list.field('worlds', {
+    //   type: 'World',
+    //   resolve(_root, _args, ctx) {
+    //     return ctx.db.world.findMany()
+    //   },
+    // })
+  },
+})
+
+schema.mutationType({
+  definition(t) {
+    t.crud.createOneUser()
+
+    t.field('createPost', {
+      type: 'Post',
+      args: {
+        title: schema.stringArg({ required: true }),
+      },
+      resolve: async (_parent, args, ctx) => {
+        return ctx.db.post.create({
+          data: {
+            title: args.title.toUpperCase(),
+            body: 'body',
+            author: {
+              connect: {
+                email: 'ro@bin.com',
+              },
+            },
+            published: true,
+          },
+        })
+      },
+    })
+  },
+})

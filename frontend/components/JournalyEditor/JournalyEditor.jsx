5e31faf6e42e5a4eeca3b35ddc6b4e50420aaadc
Robin MacPherson <robin@kumabrand.com>
2020-03-02 08:43:25 -0800

    Start over and just get the basic editor working with docs

--- frontend/components/JournalyEditor/JournalyEditor.jsx
+++ frontend/components/JournalyEditor/JournalyEditor.jsx
@@ -1,30 +1,24 @@
-import React, { useEffect, useMemo, useState, useCallback } from "react";
-import { createEditor, Editor, Transforms } from "slate";
+import React, { useEffect, useMemo, useState } from "react";
+// The Slate editor factory.
+import { createEditor } from "slate";
+// Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
 
-const CodeElement = props => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  );
-};
-
-const DefaultElement = props => {
-  return <p {...props.attributes}>{props.children}</p>;
-};
-
-// Define a React component to render leaves with bold text.
-const Leaf = props => {
-  return (
-    <span
-      {...props.attributes}
-      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
-    >
-      {props.children}
-    </span>
-  );
-};
+/**
+ * The Journaly Rich Text Editor
+ *   Uses the Slate library to build a delightgul
+ *   rich text editing experience that allows users
+ *   to craft & format beautiful posts themselves.
+ *
+ * 1. `editor` @object
+ * 2. State for the editor
+ * 3. <Slate> context provider.
+ *    - This keeps track of the editor, its plugins,
+ *      value, selection, and any changes that occur.
+ *    - It MUST be rendered above any <Editable> components.
+ *    - It can provide the editor state to other components
+ *      using the `useSlate` hook.
+ */
 
 const JournalyEditor = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
@@ -35,77 +29,10 @@ const JournalyEditor = () => {
     }
   ]);
 
-  const renderElement = useCallback(props => {
-    switch (props.element.type) {
-      case "code":
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
-    }
-  }, []);
-
-  // Define a leaf rendering function that is memoized with `useCallback`.
-  const renderLeaf = useCallback(props => {
-    return <Leaf {...props} />;
-  }, []);
-
   return (
-    <div className="journaly-editor-wrapper">
-      <div className="journaly-editor-container">
-        <Slate
-          editor={editor}
-          value={value}
-          onChange={value => setValue(value)}
-        >
-          <Editable
-            renderElement={renderElement}
-            // Pass in the `renderLeaf` function.
-            renderLeaf={renderLeaf}
-            onKeyDown={event => {
-              if (!event.ctrlKey) {
-                return;
-              }
-
-              switch (event.key) {
-                case "`": {
-                  event.preventDefault();
-                  const [match] = Editor.nodes(editor, {
-                    match: n => n.type === "code"
-                  });
-                  Transforms.setNodes(
-                    editor,
-                    { type: match ? null : "code" },
-                    { match: n => Editor.isBlock(editor, n) }
-                  );
-                  break;
-                }
-
-                case "b": {
-                  event.preventDefault();
-                  Transforms.setNodes(
-                    editor,
-                    { bold: true },
-                    { match: n => Text.isText(n), split: true }
-                  );
-                  break;
-                }
-              }
-            }}
-          />
-        </Slate>
-      </div>
-      <style jsx>{`
-        .journaly-editor-wrapper {
-          padding: 50px;
-        }
-        .journaly-editor-container {
-          padding: 0 25px;
-          border: 1px solid black;
-          border-radius: 5px;
-          min-height: 200px;
-        }
-      `}</style>
-    </div>
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Editable />
+    </Slate>
   );
 };
 

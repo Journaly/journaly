1fd3701dd2b6da335f13a35a7e355f2225a15b51
Robin MacPherson <robin@kumabrand.com>
2020-03-06 09:29:22 -0800

    Initial cookieParser & jwt config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,18 @@
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
 import { server } from 'nexus-future'
 
 server.custom(({ express }) => {
-  express.use(cors())
+  express.use(cookieParser())
+  express.use((request, response, next) => {
+    const { token } = request.cookies
+
+    if (token) {
+      const { userId } = jwt.verify(token, process.env.APP_SECRET)
+      request.userId = userId
+    }
+    next()
+  })
+  // express.use(cors())
 })

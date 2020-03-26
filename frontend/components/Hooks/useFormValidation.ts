88c05e6947b16b211d4a71602b18a5992b09db3b
Robin MacPherson <robin@kumabrand.com>
2020-03-25 21:43:57 -0700

    Add validation and build new handle functions

--- frontend/components/Hooks/useFormValidation.ts
+++ frontend/components/Hooks/useFormValidation.ts
@@ -1,7 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-function useFormValidation(initialState: object) {
+function useFormValidation(initialState: object, validate: (object) => {}) {
   const [values, setValues] = useState(initialState)
+  const [errors, setErrors] = useState({})
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  useEffect(() => {
+    if (isSubmitting) {
+      const noErrors = Object.keys(errors).length === 0
+
+      if (noErrors) {
+        console.log('authenticated!', values)
+        setIsSubmitting(false)
+      } else {
+        setIsSubmitting(false)
+      }
+    }
+  }, [errors])
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setValues({
@@ -10,9 +25,26 @@ function useFormValidation(initialState: object) {
     })
   }
 
+  function handleBlur(e: React.FormEvent<HTMLFormElement>) {
+    const validationErrors = validate(values)
+    setErrors(validationErrors)
+    console.log('Blurrrr!')
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const validationErrors = validate(values)
+    setErrors(validationErrors)
+    setIsSubmitting(true)
+  }
+
   return {
     handleChange,
     values,
+    handleSubmit,
+    handleBlur,
+    errors,
+    isSubmitting,
   }
 }
 
--- frontend/components/SignUpForm/SignUpForm.tsx
+++ frontend/components/SignUpForm/SignUpForm.tsx
@@ -1,4 +1,5 @@
 import useFormValidation from '../Hooks/useFormValidation'
+import validateAuth from '../../lib/validateAuth'
 
 const INITIAL_STATE = {
   email: '',
@@ -6,30 +7,40 @@ const INITIAL_STATE = {
 }
 
 const SignupForm: React.FC = () => {
-  const { handleChange, values } = useFormValidation(INITIAL_STATE)
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log('authenticated!', values)
-  }
+  const {
+    handleChange,
+    values,
+    handleSubmit,
+    handleBlur,
+    errors,
+    isSubmitting,
+  } = useFormValidation(INITIAL_STATE, validateAuth)
 
   return (
     <div className="form-container">
       <h1>Sign up!</h1>
       <form onSubmit={handleSubmit}>
+        {errors.email && <p>{errors.email}</p>}
         <input
           name="email"
           value={values.email}
           placeholder="Your email address"
           onChange={handleChange}
+          onBlur={handleBlur}
+          className={errors.email && 'input-error'}
         />
+        {errors.password && <p>{errors.password}</p>}
         <input
           name="password"
           value={values.password}
           placeholder="A safe password"
           onChange={handleChange}
+          onBlur={handleBlur}
+          className={errors.password && 'input-error'}
         />
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
       </form>
     </div>
   )

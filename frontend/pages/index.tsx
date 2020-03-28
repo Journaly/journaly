0a7c9dd6759f0802465702cd9c95fb69e814011e
Robin MacPherson <robin@kumabrand.com>
2020-03-28 14:58:22 -0700

    Set up TypeScript Apollo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,12 @@
-import { NextPage } from "next";
-import LandingPageLayout from "../components/Layouts/LandingPageLayout";
-import Home from "../components/Site/Home";
+import { NextPage } from 'next'
+import { withApollo } from '../lib/apollo'
+import LandingPageLayout from '../components/Layouts/LandingPageLayout'
+import Home from '../components/Site/Home'
 
 const HomePage: NextPage = () => (
   <LandingPageLayout>
     <Home />
   </LandingPageLayout>
-);
+)
 
-export default HomePage;
+export default withApollo(HomePage)

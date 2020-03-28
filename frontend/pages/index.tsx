78271c68aee3829de1d95eb9cfcc4946fa19a08f
Robin MacPherson <robin@kumabrand.com>
2020-03-28 16:50:53 -0700

    Set up codegen

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,10 +3,35 @@ import { withApollo } from '../lib/apollo'
 import LandingPageLayout from '../components/Layouts/LandingPageLayout'
 import Home from '../components/Site/Home'
 
-const HomePage: NextPage = () => (
-  <LandingPageLayout>
-    <Home />
-  </LandingPageLayout>
-)
+import { gql, useQuery } from '@apollo/client'
+
+// interface InitialProps {}
+
+// interface Props extends InitialProps {}
+
+const HomePage: NextPage = () => {
+  const { loading, error, data } = useQuery(feedQuery)
+
+  if (loading) {
+    return <p>Loading...</p>
+  } else if (error) {
+    return <p>An error occurred...</p>
+  }
+
+  const posts = data?.feed
+
+  console.log(data.feed)
+
+  return (
+    <LandingPageLayout>
+      <Home />
+      {posts.length > 0 ? (
+        posts.map(post => <p key={post.Id}>{post.Title}</p>)
+      ) : (
+        <p>Nothing to see here...</p>
+      )}
+    </LandingPageLayout>
+  )
+}
 
 export default withApollo(HomePage)

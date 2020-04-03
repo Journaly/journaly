8fb0f6124ba6e5616ff77a35d81b061d93fc4742
Robin MacPherson <robin@kumabrand.com>
2020-04-03 15:19:10 -0700

    Conditionally render & style nav based on auth

--- frontend/components/Dashboard/Nav/Nav.tsx
+++ frontend/components/Dashboard/Nav/Nav.tsx
@@ -13,60 +13,63 @@ const Nav = () => {
     return <p>An error occurred...</p>
   }
 
-  console.log(data)
+  const currentUser = data.currentUser
 
   return (
     <nav>
-      <div className="nav-top">
-        <Link href="/dashboard/profile">
-          <a>
-            <img className="profile-img" src="/images/robin-small.png" />
-            <p className="current-user-name">Robin MacPherson</p>
-          </a>
-        </Link>
-      </div>
+      {currentUser && (
+        <>
+          <div className="nav-top">
+            <Link href="/dashboard/profile">
+              <a>
+                <img className="profile-img" src="/images/robin-small.png" />
+                <p className="current-user-name">Robin MacPherson</p>
+              </a>
+            </Link>
+          </div>
+          <div className="nav-bottom">
+            <Link href="/dashboard/my-feed">
+              <a className="nav-link">
+                <img src="../images/icons/your-feed-icon.svg" alt="" />
+                <span className="nav-link-text">My Feed</span>
+              </a>
+            </Link>
+            <Link href="/dashboard/my-posts">
+              <a className="nav-link">
+                <img src="../images/icons/your-feed-icon.svg" alt="" />
+                <span className="nav-link-text">My Posts</span>
+              </a>
+            </Link>
+            <Link href="/dashboard/new-post">
+              <a className="nav-link">
+                <img src="../images/icons/new-post-icon.svg" alt="" />
+                <span className="nav-link-text">New Post</span>
+              </a>
+            </Link>
+            <Link href="/dashboard/drafts">
+              <a className="nav-link">
+                <img src="../images/icons/drafts-icon.svg" alt="" />
+                <span className="nav-link-text">Drafts</span>
+              </a>
+            </Link>
 
-      <div className="nav-bottom">
-        <Link href="/my-feed">
-          <a className="nav-link">
-            <img src="../images/icons/your-feed-icon.svg" alt="" />
-            <span className="nav-link-text">My Feed</span>
-          </a>
-        </Link>
-        <Link href="/dashboard/my-posts">
-          <a className="nav-link">
-            <img src="../images/icons/your-feed-icon.svg" alt="" />
-            <span className="nav-link-text">My Posts</span>
-          </a>
-        </Link>
-        <Link href="/dashboard/new-post">
-          <a className="nav-link">
-            <img src="../images/icons/new-post-icon.svg" alt="" />
-            <span className="nav-link-text">New Post</span>
-          </a>
-        </Link>
-        <Link href="/dashboard/drafts">
-          <a className="nav-link">
-            <img src="../images/icons/drafts-icon.svg" alt="" />
-            <span className="nav-link-text">Drafts</span>
-          </a>
-        </Link>
+            <hr />
 
-        <hr />
-
-        <Link href="/dashboard/settings">
-          <a className="nav-link">
-            <img src="../images/icons/settings-icon.svg" alt="" />
-            <span className="nav-link-text">Settings</span>
-          </a>
-        </Link>
-        <Link href="/">
-          <a className="log-out nav-link">
-            <img src="../images/icons/logout-icon.svg" alt="Log out" />
-            <span className="nav-link-text">Log Out</span>
-          </a>
-        </Link>
-      </div>
+            <Link href="/dashboard/settings">
+              <a className="nav-link">
+                <img src="../images/icons/settings-icon.svg" alt="" />
+                <span className="nav-link-text">Settings</span>
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="log-out nav-link">
+                <img src="../images/icons/logout-icon.svg" alt="Log out" />
+                <span className="nav-link-text">Log Out</span>
+              </a>
+            </Link>
+          </div>
+        </>
+      )}
       <h1>
         <Link href="/">
           <a className="nav-logo">
@@ -146,7 +149,7 @@ const Nav = () => {
           align-items: center;
           justify-content: center;
           width: 100%;
-          margin-right: 15px;
+          margin: ${currentUser ? '0 15px 0 0' : '50vh 15px 0 0'};
           font-size: 24px;
           color: white;
         }

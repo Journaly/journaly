20b77e3b1fb5e088030fb21ca7902f5138a5d8af
Robin MacPherson <robin@kumabrand.com>
2020-04-05 10:19:43 -0700

    Add gtag to custom _app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
+import Router from 'next/router'
+
 import '../styles/reset.css'
 import '../styles/globalStyles.css'
+import * as gtag from '../lib/gtag'
+
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 export default function MyApp({ Component, pageProps }) {
   return (

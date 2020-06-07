1a62c04cad2272d22a4a3971d284089f85c7ae4d
Nick Litwin <nicklitwin88@gmail.com>
2020-06-06 17:40:23 -0700

    Add mobile styles for the settings tabs

--- frontend/components/Layouts/SettingsPageLayout.tsx
+++ frontend/components/Layouts/SettingsPageLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import DashboardLayout from './DashboardLayout'
 import Breadcrumbs from '../Dashboard/Settings/Breadcrumbs'
 import SettingsNav from '../Dashboard/Settings/SettingsNav'
+import theme from '../../theme'
 
 interface Props {
   children: React.ReactNode
@@ -21,8 +22,14 @@ const SettingsPageLayout: React.FC<Props> = ({ children }) => {
       <style jsx>{`
         .settings-container {
           display: flex;
-
-          margin-top: 65px;
+          flex-direction: column;
+          margin-top: 35px;
+        }
+        @media (min-width: ${theme.breakpoints.SM}) {
+          .settings-container {
+            flex-direction: row;
+            margin-top: 65px;
+          }
         }
 
         .settings-container :global(.settings-nav) {

b9218f686d3e3e09c353f83dad50f1da937783a6
Nick Litwin <nicklitwin88@gmail.com>
2020-06-30 09:25:21 -0700

    Add TabToggle to my-posts page

--- frontend/pages/dashboard/my-posts.tsx
+++ frontend/pages/dashboard/my-posts.tsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NextPage } from 'next'
-
 import { withApollo } from '../../lib/apollo'
 import DashboardLayout from '../../components/Layouts/DashboardLayout'
+import TabToggle from '../../elements/TabToggle'
 
 const MyPostsPage: NextPage = () => {
+  const tabs = [
+    { key: 'my-posts', text: 'My Posts' },
+    { key: 'drafts', text: 'Drafts' },
+  ]
+  const [activeKey, setActiveKey] = useState(tabs[0].key)
+
+  const handleToggle = (key: string): void => {
+    // TODO(nick): wire up query params so you can directly link to a tab
+    setActiveKey(key)
+  }
+
   return (
     <DashboardLayout>
-      <h1>My Posts</h1>
+      <TabToggle activeKey={activeKey} tabs={tabs} onToggle={handleToggle} />
+
+      <div className="posts-wrapper">
+        {activeKey === 'my-posts' && <div>My Posts</div>}
+        {activeKey === 'drafts' && <div>Drafts</div>}
+      </div>
+
       <style jsx>{`
-        h1 {
-          margin: 50px auto;
+        :global(.tab-toggle) {
+          margin: 0 auto;
+        }
+        .posts-wrapper {
+          margin: 0 auto 50px;
+          padding: 50px 200px;
+        }
+
+        @keyframes fadeIn {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+
+        .posts-wrapper > div {
+          padding: 50px;
+          animation: 150ms fadeIn ease-in;
         }
       `}</style>
     </DashboardLayout>

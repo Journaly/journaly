984f094a2ba4dedbce10655c90a05a9cd61a93a5
Robin MacPherson <robin@kumabrand.com>
2020-06-29 21:26:19 -0700

    Add basic commands

---
+++ cypress/support/commands.js
@@ -0,0 +1,43 @@
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+Cypress.Commands.add('login', () => {
+  cy.get('[data-test=email]').type('j@n.com')
+  cy.get('[data-test=password]').type('password{enter}')
+  cy.url().should('include', '/dashboard/my-feed')
+})
+
+// TODO (robin-macpherson): Try to get something like this to work
+// so that we build up state programmatically and don't build up state in the UI.
+// Cypress.Commands.add('login', () => {
+//   cy.request({
+//     method: 'POST',
+//     url: 'http://localhost:4000/graphql',
+//     body: {
+//       operationName: 'loginUser',
+//       query: '...',
+//       variables: {
+//         identifier: 'j@n.com',
+//         password: 'password',
+//       },
+//     },
+//   })
+// })
+
+Cypress.Commands.add('signup', () => {
+  cy.get('[data-test=display-name]').type('smartest_man_in_westeros_91')
+  cy.get('[data-test=email]').type('tyri@n.com')
+  cy.get('[data-test=password]').type('password{enter}')
+  cy.url().should('include', '/dashboard/my-feed')
+})
+
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })

52c61f055c891bc15c75839d70f0175255079e4e
Robin MacPherson <robin@kumabrand.com>
2020-06-29 21:50:44 -0700

    Clean up sample landing page test

--- cypress/integration/landing/explore_landing_page.spec.js
+++ cypress/integration/landing/explore_landing_page.spec.js
@@ -1,7 +1,6 @@
 describe('Explores the Journaly landing page', () => {
   it('Visits the landing page', () => {
     cy.visit('http://localhost:3000')
-    cy.contains('Journaly').click()
-    cy.url().should('include', '/post/2')
+    cy.contains('Journaly')
   })
 })

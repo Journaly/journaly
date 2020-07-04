59138f50f66e1926b41e6626ac41a8816c9f8e66
Nick Litwin <nicklitwin88@gmail.com>
2020-07-03 21:27:03 -0700

    Add images for the profile page and posts

--- frontend/components/Icons/XIcon.tsx
+++ frontend/components/Icons/XIcon.tsx
@@ -3,16 +3,26 @@ interface SVGRProps {
   title?: string
   titleId?: string
   size?: number
+  color?: string
 }
 
-function XIcon({ title, titleId, size, ...props }: React.SVGProps<SVGSVGElement> & SVGRProps) {
+function XIcon({
+  title,
+  titleId,
+  size,
+  color,
+  ...props
+}: React.SVGProps<SVGSVGElement> & SVGRProps) {
   if (!size) size = 24
 
   return (
     <svg width={size} height={size} viewBox="0 0 24 24" aria-labelledby={titleId} {...props}>
       {title ? <title id={titleId}>{title}</title> : null}
       <path fill="none" d="M0 0h24v24H0V0z" />
-      <path d="M18.3 5.71a.996.996 0 00-1.41 0L12 10.59 7.11 5.7A.996.996 0 105.7 7.11L10.59 12 5.7 16.89a.996.996 0 101.41 1.41L12 13.41l4.89 4.89a.996.996 0 101.41-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z" />
+      <path
+        fill={color}
+        d="M18.3 5.71a.996.996 0 00-1.41 0L12 10.59 7.11 5.7A.996.996 0 105.7 7.11L10.59 12 5.7 16.89a.996.996 0 101.41 1.41L12 13.41l4.89 4.89a.996.996 0 101.41-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"
+      />
     </svg>
   )
 }
---
+++ frontend/components/Icons/YoutubeIcon.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+interface SVGRProps {
+  title?: string
+  titleId?: string
+  size?: number
+}
+
+function YoutubeIcon({
+  title,
+  titleId,
+  size = 60,
+  ...props
+}: React.SVGProps<SVGSVGElement> & SVGRProps) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 60 60" aria-labelledby={titleId} {...props}>
+      {title ? <title id={titleId}>{title}</title> : null}
+      <g data-name="Group 437">
+        <g data-name="Ellipse 5" fill="none" stroke="#fff" strokeWidth={2}>
+          <circle cx={30} cy={30} r={30} stroke="none" />
+          <circle cx={30} cy={30} r={29} />
+        </g>
+        <path
+          data-name="Path 259"
+          d="M44.983 27.203q-.016-.9-.142-2.285a20.784 20.784 0 00-.36-2.469 3.893 3.893 0 00-1.163-2.059 3.568 3.568 0 00-2.084-.971A109.83 109.83 0 0030 19a109.826 109.826 0 00-11.233.418 3.53 3.53 0 00-2.076.971 3.915 3.915 0 00-1.155 2.06 18.529 18.529 0 00-.377 2.469q-.125 1.381-.142 2.285T15 29.712q0 1.607.017 2.511t.142 2.289a20.707 20.707 0 00.36 2.47 3.891 3.891 0 001.163 2.059 3.57 3.57 0 002.084.971A109.72 109.72 0 0030 40.429a109.721 109.721 0 0011.233-.417 3.53 3.53 0 002.076-.971 3.916 3.916 0 001.155-2.059 18.541 18.541 0 00.377-2.47q.125-1.381.142-2.285T45 29.712q0-1.6-.017-2.509zm-9.057 3.415l-8.571 5.357a.955.955 0 01-.569.167 1.158 1.158 0 01-.519-.134 1 1 0 01-.552-.937V24.357a1 1 0 01.552-.937 1.008 1.008 0 011.088.033l8.571 5.359a1.065 1.065 0 010 1.808z"
+          fill="#fff"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default YoutubeIcon

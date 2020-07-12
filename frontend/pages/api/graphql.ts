35c6c10dad002d2a8c886a471be4bdb59ea1789c
Ryan Lanny Jenkins <rjenkins@styleseat.com>
2020-07-12 14:58:04 -0500

    Move source files out of be/src into nexus/

--- frontend/pages/api/graphql.ts
+++ frontend/pages/api/graphql.ts
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'development') require('nexus').default.reset()
 
 const app = require('nexus').default
 
-require('../../backend/src/graphql')
+require('../../nexus/graphql')
 
 // Watch https://github.com/graphql-nexus/nexus/issues/524
 // and https://github.com/graphql-nexus/nexus/issues/523 for future
--- frontend/pages/api/playground.ts
+++ frontend/pages/api/playground.ts
@@ -1,9 +1,17 @@
-if (process.env.NODE_ENV === 'development') require('nexus').default.reset()
+let handler
 
-const app = require('nexus').default
+if (process.env.NODE_ENV === 'development') {
+  require('nexus').default.reset()
+  const app = require('nexus').default
+  require('../../nexus/graphql')
 
-require('../../backend/src/graphql')
+  app.assemble()
+  handler = app.server.handlers.playground
+} else {
+  // Disable playground outside of development
+  handler = (_: any, res: any) => {
+    res.status(403).send('GQL playground is disabled outside of development')
+  }
+}
 
-app.assemble()
-
-export default app.server.handlers.playground
+export default handler

1d8fc569fe54560179828930347c033d1747688d
Nick Litwin <nicklitwin88@gmail.com>
2020-07-26 11:20:09 -0700

    Update styles of language select

--- frontend/components/LanguageSelect/index.tsx
+++ frontend/components/LanguageSelect/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import CSS from 'csstype'
+import classNames from 'classnames'
 
 import { LanguageFragmentFragment as LanguageType } from '../../generated/graphql'
+import ChevronIcon from '../Icons/ChevronIcon'
 
 type LanguageSelectProps = {
   languages: LanguageType[]
   value: number
   onChange: (newValue: number) => void
-  id?: string | undefined,
-  style?: CSS.Properties | undefined,
+  id?: string
+  className?: string
 }
 
 const LanguageSelect: React.FC<LanguageSelectProps> = ({
@@ -16,23 +17,49 @@ const LanguageSelect: React.FC<LanguageSelectProps> = ({
   value,
   onChange,
   id,
-  style,
+  className,
 }: LanguageSelectProps) => {
   return (
-    <select
-      id={id}
-      value={value}
-      style={style}
-      className="j-field"
-      onChange={(e) => onChange(parseInt(e.target.value, 10))}
-    >
-      <option value={-1}>Select language...</option>
-      {languages.map((lang) => (
-        <option value={lang.id} key={lang.id}>
-          {lang.name} {lang.dialect && `(${lang.dialect})`}
-        </option>
-      ))}
-    </select>
+    <div className={classNames('language-select-container', className)}>
+      <select
+        id={id}
+        value={value}
+        className="j-field"
+        onChange={(e) => onChange(parseInt(e.target.value, 10))}
+      >
+        <option value={-1}>Select language...</option>
+        {languages.map((lang) => (
+          <option value={lang.id} key={lang.id}>
+            {lang.name} {lang.dialect && `(${lang.dialect})`}
+          </option>
+        ))}
+      </select>
+
+      <ChevronIcon className="select-arrow" />
+
+      <style jsx>{`
+        .language-select-container {
+          position: relative;
+          width: 100%;
+        }
+
+        select {
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          appearance: none;
+          width: 100%;
+        }
+
+        .language-select-container :global(svg) {
+          position: absolute;
+          display: block;
+          right: 0;
+          bottom: 7px;
+          padding: 0;
+          pointer-events: none;
+        }
+      `}</style>
+    </div>
   )
 }
 

147ad43886e3ebda39f4f7deec67580653cdda24
Robin MacPherson <robin@kumabrand.com>
2020-07-30 10:02:51 -0700

    Create mail.ts

---
+++ frontend/lib/mail.ts
@@ -0,0 +1,26 @@
+import nodemailer from 'nodemailer'
+
+export const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST as string,
+  port: parseInt(process.env.MAIL_PORT || '25', 10),
+  auth: {
+    user: process.env.MAIL_USER as string,
+    pass: process.env.MAIL_PASS as string,
+  },
+  secure: true,
+})
+
+export const makeEmail = (text: string) => `
+<div className="email" style="
+  border: 1px solid black;
+  padding: 20px;
+  font-family: sans-serif;
+  line-height: 2;
+font-size: 20px;
+">
+  <h2>Howdy, Journaler!</h2>
+  <p>${text}</p>
+
+  <p>Robin &commat; Journaly</p>
+</div>
+`

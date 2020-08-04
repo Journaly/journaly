2e3bdc103fdf120d86583ad00b04aee8d0139e91
Robin MacPherson <robin@kumabrand.com>
2020-08-04 10:57:14 -0700

    Clean up

--- frontend/components/Dashboard/Post/Post.tsx
+++ frontend/components/Dashboard/Post/Post.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import { sanitize, formatLongDate } from '../../../utils'
+import { sanitize } from '../../../utils'
 
 import {
   Post as PostType,

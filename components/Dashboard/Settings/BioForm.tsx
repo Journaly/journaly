c9a22c0e90a6857762669d02ae424e5342edd4bc
Nick Litwin <nicklitwin88@gmail.com>
2020-08-16 07:42:54 -0700

    Add toast success messages for updating your bio

--- components/Dashboard/Settings/BioForm.tsx
+++ components/Dashboard/Settings/BioForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 import { sanitize } from '../../../utils'
 import { useTranslation } from '../../../config/i18n'
 import SettingsForm from '../../../components/Dashboard/Settings/SettingsForm'
@@ -17,7 +18,14 @@ type FormValues = {
 
 const BioForm: React.FC<Props> = ({ bio }) => {
   const { t } = useTranslation('settings')
-  const [updateUser, { loading }] = useUpdateUserMutation()
+  const [updateUser, { loading }] = useUpdateUserMutation({
+    onCompleted: () => {
+      toast.success(t('profile.bio.bioSuccess'))
+    },
+    onError: () => {
+      toast.error(t('profile.error.updateError'))
+    },
+  })
   const { handleSubmit, register } = useForm<FormValues>({
     mode: 'onSubmit',
     reValidateMode: 'onBlur',

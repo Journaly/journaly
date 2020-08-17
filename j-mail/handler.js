2160d0d9561ce9eee50540d2da44abcc3efaa2f3
Robin MacPherson <robin@kumabrand.com>
2020-08-16 19:54:31 -0700

    First try at getting set up

---
+++ j-mail/handler.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import AWS from 'aws-sdk'
+const sqs = new AWS.SQS({ region: 'us-west-1' })
+
+const AWS_ACCOUNT = process.env.ACCOUNT_ID
+const QUEUE_URL = `https://sqs.us-west-1.amazonaws.com/${AWS_ACCOUNT}/JMailQueue`
+
+module.exports.hello = (event, context, callback) => {
+  const params = {
+    MessageBody: 'Hello!',
+    QueueUrl: QUEUE_URL,
+  }
+
+  sqs.sendMessage(params, function (err, data) {
+    if (err) {
+      console.log('error', 'Failed to send message' + err)
+
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'ERROR',
+        }),
+      }
+
+      callback(null, response)
+    } else {
+      console.log('data:', data.MessageId)
+
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: data.MessageId,
+        }),
+      }
+    }
+  })
+}
+
+module.exports.sqsHello = (event, context, callback) => {
+  console.log('It was called!')
+  console.log(event)
+  context.done(null, '')
+}

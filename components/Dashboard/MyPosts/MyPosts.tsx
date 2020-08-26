484cedfeaf854791c337673bc59205af87e9eaac
Nick Litwin <nicklitwin88@gmail.com>
2020-08-26 11:40:30 -0700

    Add comment to explain why the more complicated grid-template-column is needed
    https://stackoverflow.com/a/61240964/2918915

--- components/Dashboard/MyPosts/MyPosts.tsx
+++ components/Dashboard/MyPosts/MyPosts.tsx
@@ -75,6 +75,7 @@ const MyPosts: React.FC<Props> = ({ currentUser, status }) => {
 
         @media (min-width: ${theme.breakpoints.SM}) {
           .my-posts {
+            /* Ensure both columns have equal width */
             grid-template-columns: repeat(2, minmax(0, 1fr));
           }
         }

73c2dda466621c6873bc6d2834b162e01be9e6d9
Robin MacPherson <robin@kumabrand.com>
2020-09-01 08:51:32 -0700

    Get started

--- nexus/like.ts
+++ nexus/like.ts
@@ -6,3 +6,12 @@ schema.objectType({
     t.model.id()
   },
 })
+
+schema.objectType({
+  name: 'CommentLike',
+  definition(t) {
+    t.model.id()
+    t.model.commentId()
+    t.model.author()
+  },
+})

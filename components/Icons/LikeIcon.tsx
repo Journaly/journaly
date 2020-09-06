26606bf2cf0f60f24704b4b0488d9257418d0164
Robin MacPherson <robin@kumabrand.com>
2020-09-06 14:23:36 -0700

    Add fill options to LikeIcon

--- components/Icons/LikeIcon.tsx
+++ components/Icons/LikeIcon.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react'
+import theme from '../../theme'
 interface SVGRProps {
   title?: string
   titleId?: string
+  filled: boolean
 }
 
-function LikeIcon({ title, titleId, ...props }: React.SVGProps<SVGSVGElement> & SVGRProps) {
+function LikeIcon({
+  title,
+  titleId,
+  filled = false,
+  ...props
+}: React.SVGProps<SVGSVGElement> & SVGRProps) {
   return (
     <svg width={16} height={14.119} viewBox="0 0 16 14.119" aria-labelledby={titleId} {...props}>
       {title ? <title id={titleId}>{title}</title> : null}
       <path
         d="M14.721 1.275a4.3 4.3 0 00-6.119 0l-.6.6-.6-.6a4.331 4.331 0 00-6.127 6.123l6.721 6.721 6.721-6.721a4.3 4.3 0 000-6.119"
-        fill="#4391c9"
+        fill={filled ? theme.colors.blueLight : theme.colors.white}
         fillRule="evenodd"
         data-name="Group 383"
+        stroke="#4391c9"
       />
     </svg>
   )

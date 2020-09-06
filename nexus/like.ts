4460b575f0e3fa3da8caf457bf003ba1a2598e6a
Robin MacPherson <robin@kumabrand.com>
2020-09-06 13:01:00 -0700

    Draft the createCommentLike Mutation

--- nexus/like.ts
+++ nexus/like.ts
@@ -1,4 +1,8 @@
 import { schema } from 'nexus'
+import { sendCommentLikeNotification } from './utils'
+import { threadId } from 'worker_threads'
+
+const { intArg } = schema
 
 schema.objectType({
   name: 'PostLike',
@@ -13,5 +17,71 @@ schema.objectType({
     t.model.id()
     t.model.commentId()
     t.model.author()
+  },
+})
+
+schema.mutationType({
+  definition(t) {
+    t.field('createCommentLike', {
+      type: 'CommentLike',
+      args: {
+        commentId: intArg({ required: true }),
+      },
+      resolve: async (parent, args, ctx) => {
+        const { userId } = ctx.request
+        if (!userId) {
+          throw new Error('You must be logged in to create threads.')
+        }
+
+        const { commentId } = args
+        const comment = await ctx.db.comment.findOne({
+          where: {
+            id: commentId,
+          },
+          include: {
+            author: true,
+            thread: true,
+          },
+        })
+
+        const post = await ctx.db.post.findOne({
+          where: {
+            id: comment?.thread.postId,
+          },
+        })
+
+        if (!comment || !post) {
+          throw new Error('Comment or Post record not found.')
+        }
+
+        // check if user has already liked this comment
+        for (let like of comment.likes) {
+          if (like.userId === userId) return
+        }
+
+        const commentLike = await ctx.db.commentLike.create({
+          data: {
+            author: {
+              connect: { id: userId },
+            },
+            comment: {
+              connect: {
+                id: comment.id,
+              },
+            },
+          },
+        })
+
+        await sendCommentLikeNotification({
+          post,
+          thread: comment.thread,
+          comment,
+          commentAuthor: comment.author,
+          commentLikeAuthor: commentLike.author,
+        })
+
+        return commentLike
+      },
+    })
   },
 })

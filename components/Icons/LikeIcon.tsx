6169118e5ba7f84dccb19ddadeb2718429be77ee
Robin MacPherson <robin@kumabrand.com>
2020-09-08 14:27:00 -0700

    Fix fractional sizing

--- components/Icons/LikeIcon.tsx
+++ components/Icons/LikeIcon.tsx
@@ -13,13 +13,7 @@ function LikeIcon({
   ...props
 }: React.SVGProps<SVGSVGElement> & SVGRProps) {
   return (
-    <svg
-      width={16}
-      height={14.119}
-      viewBox="-0.5 -0.5 17 15.119"
-      aria-labelledby={titleId}
-      {...props}
-    >
+    <svg width={16} height={14} viewBox="-0.5 -0.5 17 15" aria-labelledby={titleId} {...props}>
       {title ? <title id={titleId}>{title}</title> : null}
       <path
         d="M14.721 1.275a4.3 4.3 0 00-6.119 0l-.6.6-.6-.6a4.331 4.331 0 00-6.127 6.123l6.721 6.721 6.721-6.721a4.3 4.3 0 000-6.119"

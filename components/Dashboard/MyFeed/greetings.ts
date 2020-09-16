5d90a2d754398e65b30f4c4e0d4837a3f03883c8
Robin MacPherson <robin@kumabrand.com>
2020-09-16 10:51:39 -0700

    Create greetings TS file and remove those strings from translation file

---
+++ components/Dashboard/MyFeed/greetings.ts
@@ -0,0 +1,13 @@
+export const greetings = {
+  English: 'Hello',
+  French: 'Bonjour',
+  Spanish: 'Hola',
+  Portuguese: 'Oi',
+  German: 'Hallo',
+  Japanese: 'こんにちは',
+  Korean: '',
+  Persian: '',
+  Hungarian: '',
+  Afrikaans: '',
+  Arabic: '',
+}

664fe2d90b716e59e478548f53b524e6d47ba1a3
Robin MacPherson <robin@kumabrand.com>
2020-09-19 15:50:23 -0700

    Write all resetPassword things

---
+++ pages/dashboard/reset-password.tsx
@@ -0,0 +1,28 @@
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { withApollo } from '../../lib/apollo'
+import ResetPasswordForm from '../../components/ResetPasswordForm'
+import LandingPageLayout from '../../components/Layouts/LandingPageLayout'
+
+const ResetPasswordPage: NextPage = () => {
+  const {
+    query: { resetToken },
+  } = useRouter()
+
+  return (
+    <LandingPageLayout>
+      <ResetPasswordForm resetToken={resetToken} />
+      <style jsx>{`
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+        grid-gap: 20px;
+      `}</style>
+    </LandingPageLayout>
+  )
+}
+
+ResetPasswordPage.getInitialProps = async () => ({
+  namespacesRequired: [],
+})
+
+export default withApollo(ResetPasswordPage)

c2e44abaeb28665dedc3ec57a17b8b3f2ede0e05
Robin MacPherson <hello@robinmacpherson.co>
2020-12-28 13:58:45 -0800

    Update components/Dashboard/Settings/DetailsForm.tsx
    Co-authored-by: Ryan Jenkins <2343714+Lanny@users.noreply.github.com>

--- components/Dashboard/Settings/DetailsForm.tsx
+++ components/Dashboard/Settings/DetailsForm.tsx
@@ -220,7 +220,7 @@ const DetailsForm: React.FC<DetailsFormProps> = ({ currentUser }) => {
                   />
               </div>
               <div className="details-form-field">
-                <label className="settings-label" htmlFor="location">
+                <label className="settings-label" htmlFor="country">
                 {t('profile.details.countryLabel')}
                 </label>
                 <input

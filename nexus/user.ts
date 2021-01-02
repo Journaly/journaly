b5185a30237beea63095352c5c51eba7eac7c72d
Ryan Lanny Jenkins <rjenkins@styleseat.com>
2021-01-02 10:58:36 -0600

    Remove unused import.

--- nexus/user.ts
+++ nexus/user.ts
@@ -9,7 +9,6 @@ import jwt from 'jsonwebtoken'
 import { serialize } from 'cookie'
 import { randomBytes } from 'crypto'
 import { promisify } from 'util'
-import { BadgeType } from '@journaly/j-db-client'
 
 import { NotAuthorizedError } from './errors'
 import { sendPasswordResetTokenEmail } from './utils'

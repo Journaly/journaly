02c5f1d9ffe7a97e5175e84cf2038d2974b3671a
Robin MacPherson <robin@kumabrand.com>
2021-01-06 13:27:56 -0800

    It's coming together!!

--- j-mail/handler.ts
+++ j-mail/handler.ts
@@ -87,27 +87,32 @@ const createUpdateEmailBody = (data: DataForUpdateEmail) => {
       display: flex;
       flex-direction: column;
       align-items: center;
-      padding: 20px;
-      font-family: sans-serif;
-      line-height: 2;
+      padding: 40px 20px;
+      font-family: 'Courier New', Courier, monospace;
+      line-height: 1.2;
       background: #ebeae7;
+      margin-top: 0px;
     ">
       <div style="
-        border-top: 8px solid black;
+        border-top: 10px solid #313131;
         width: 80%;
         background: white;
+        padding: 30px 20px;
         text-align: center;
-        padding: 20px;
+        margin-bottom: 25px;
       ">
         <img
           src="https://journaly-email-assets.s3.us-east-2.amazonaws.com/j-logo-100.png"
-          style="max-width: 55px;"
+          style="width: 75px;"
         >
         <h1 style="
-          margin-top: 5;
+          margin-top: 25px;
+          margin-bottom: 25px;
           font-size: 24px;
+          font-family: Verdana, Geneva, Tahoma, sans-serif;
+          font-weight: 400;
         ">Howdy, Journaler!</h1>
-        <p style="font-size: 16px;">
+        <p style="font-size: 14px; text-align: left;">
           There's been some new activity on Journaly that we thought you'd like to know about. Here's a summary!
         </p>
       </div>
@@ -116,11 +121,10 @@ const createUpdateEmailBody = (data: DataForUpdateEmail) => {
       ${ (data.own.length && `
         <div style="
           padding: 5px 20px;
-          font-family: sans-serif;
-          line-height: 2;
-          font-size: 20px;
-          margin-top: 20px;
-          margin-bottom: 10px;
+          font-family: 'Courier New', Courier, monospace;
+          line-height: 1.2;
+          font-size: 14px;
+          margin-bottom: 25px;
           width: 80%;
           background: white;
         ">
@@ -128,6 +132,8 @@ const createUpdateEmailBody = (data: DataForUpdateEmail) => {
             text-transform: uppercase;
             text-align: center;
             font-size: 20px;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-weight: 400;
           ">Your posts:</h2>
           ${mapCat(data.own, formatNotificationBlock)}
         </div>
@@ -136,11 +142,10 @@ const createUpdateEmailBody = (data: DataForUpdateEmail) => {
       ${ (data.other.length && `
         <div style="
           padding: 5px 20px;
-          font-family: sans-serif;
-          line-height: 2;
+          font-family: 'Courier New', Courier, monospace;
+          line-height: 1.2;
           font-size: 20px;
-          margin-top: 20px;
-          margin-bottom: 10px;
+          margin-bottom: 25px;
           width: 80%;
           background: white;
         ">
@@ -148,6 +153,8 @@ const createUpdateEmailBody = (data: DataForUpdateEmail) => {
             text-transform: uppercase;
             text-align: center;
             font-size: 20px;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-weight: 400;
           ">Posts you've participated in:</h2>
           ${mapCat(data.other, formatNotificationBlock)}
         </div>
@@ -157,14 +164,27 @@ const createUpdateEmailBody = (data: DataForUpdateEmail) => {
         background: white;
         width: 80%;
         padding: 5px 20px;
+        margin-bottom: 25px;
       ">
-        <p style="font-size: 16px;">Keep up all the great work and thank you for contributing to the community!</p>
-        <p style="font-size: 16px;">Robin @ Journaly</p>
+        <p style="font-size: 14px;">Keep up all the great work and thank you for contributing to the community!</p>
+        <p style="font-size: 14px; font-weight: 600;">Robin @ Journaly</p>
       </div>
       <div style="
-        padding: 30px; 
+        padding: 40px 20px;
+        margin-bottom: 25px;
+        background-color: #313131;
+        width: 80%;
+        text-align: center; 
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-weight: 400;
+        font-size: 10px;
       ">
-        <p style="text-transform: uppercase; font-weight: 600; font-size: 16px; color: #4391C9;">Sent with ❤️ from the journaly team</p>
+      <p style="
+        text-transform: uppercase;
+        color: white;
+      ">
+        Sent with ❤️ from the <a href="https://www.journaly.com" style="color: #4391C9; text-decoration: none;">journaly</a> team
+      </p>
       </div>
     </div>
   `

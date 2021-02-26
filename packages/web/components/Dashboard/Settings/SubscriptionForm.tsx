c5e99b887a9da03e5847bbaf9facc2f76993f4f9
Robin MacPherson <robin@kumabrand.com>
2021-02-26 15:50:02 -0800

    Get things working to where we successfully get Stripe paymentMethod back

--- packages/web/components/Dashboard/Settings/SubscriptionForm.tsx
+++ packages/web/components/Dashboard/Settings/SubscriptionForm.tsx
@@ -1,86 +1,91 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'react-toastify'
-import { loadStripe } from '@stripe/stripe-js'
+import { loadStripe, StripeError } from '@stripe/stripe-js'
 import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
+import nProgress from 'nprogress'
 import Button from '@/components/Button'
 import FormError from '@/components/FormError'
 import { useTranslation } from '@/config/i18n'
 import SettingsForm from '@/components/Dashboard/Settings/SettingsForm'
 import SettingsFieldset from '@/components/Dashboard/Settings/SettingsFieldset'
-import nProgress from 'nprogress'
+import { useCreateMembershipSubscriptionMutation, MembershipSubscriptionType } from '@/generated/graphql'
 
 type FormValues = {
-  type: UserSubscriptionType
-  price: number
+  type: MembershipSubscriptionType
 }
 
-const stripeLib = loadStripe(process.env.STRIPE_PUBLIC_KEY!)
+const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
 
 const SubscriptionForm: React.FC = () => {
-  const { t } = useTranslation('settings.subscription')
-  const [customError, setCustomError] = useState()
-  const [customLoading, setCustomLoading] = useState(false)
+  const { t } = useTranslation('settings')
+  const [stripeError, setStripeError] = useState<StripeError>()
   const stripe = useStripe()
   const elements = useElements()
 
-  const { handleSubmit, register, getValues, errors } = useForm<FormValues>({
+  const { handleSubmit, errors, formState } = useForm<FormValues>({
     mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
   })
 
-  const [createUserSubscription, { loading }] = useCreateUserSubscriptionMutation({
+  const [createUserSubscription, { loading }] = useCreateMembershipSubscriptionMutation({
     onCompleted: () => {
-      toast.success('subscribeSuccessMessage')
+      toast.success(t('subscription.subscribeSuccessMessage'))
     },
     onError: () => {
-      toast.error(t('subscribeFailureMessage'))
+      toast.error(t('subscription.subscribeFailureMessage'))
     },
   })
 
   const handleSubscribeSubmit = async (data: FormValues) => {
-    setCustomLoading(true)
     nProgress.start()
-    // 3. Create payment method via stripe
-    // Token comes back here if successful
+    // 1. Create payment method via stripe
+    //    Token comes back here if successful
     if (elements) {
       const { error, paymentMethod } = await stripe?.createPaymentMethod({
         type: 'card',
         card: elements.getElement(CardElement),
       })
-      // 4. Handle any errors from sttipe
+      console.log(paymentMethod)
+      // 2. Handle any errors from sttipe
       if (error) {
-        setCustomError(error)
+        setStripeError(error)
       }
     }
-    // 5. Send token from #3 to serverside logic
-    // 6. Change the page to view the order?
-    // 7. Turn loader off
-    setCustomLoading(false)
+    // 3. Send token from #1 to serverside logic
+    // 4. Redirect to receipt/order page
+
     if (!loading && Object.keys(errors).length === 0) {
       createUserSubscription({
         variables: {
           type: data.type,
-          price: data.price,
         },
       })
     }
     nProgress.done()
   }
 
+  const errorInput = Object.values(errors)[0]
+
   return (
-    <SettingsForm
-      onSubmit={handleSubmit(handleSubscribeSubmit)}
-      errorInputName={Object.keys(errors)[0] || ''}
-    >
-      {(customError || errorInput) && <FormError error={customError.message || errorInput.message as string}/>}
-      <SettingsFieldset legend={t('legend')}>
-        <CardElement />
-        <Button
-          type="submit"
-          loading={loading}
-        >{t('subscribeCta')}</Button>
-      </SettingsFieldset>
-    </SettingsForm>
+    <>
+      <SettingsForm
+        onSubmit={handleSubmit(handleSubscribeSubmit)}
+        errorInputName={Object.keys(errors)[0] || ''}
+      >
+        {(stripeError || errorInput) && <FormError error={stripeError?.message || errorInput?.message as string}/>}
+        <SettingsFieldset legend={t('subscription.legend')}>
+          <CardElement />
+          <Button
+            type="submit"
+            loading={loading || formState.isSubmitting}
+            style={{
+              marginTop: '15px',
+            }}
+          >{t('subscription.subscribeCta')}</Button>
+        </SettingsFieldset>
+      </SettingsForm>
+    </>
   )
 }
 

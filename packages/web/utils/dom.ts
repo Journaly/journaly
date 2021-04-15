a78e52164bda2d3d4665a6dbbc4fecbe89e049f3
Ryan Lanny Jenkins <lan.rogers.book@gmail.com>
2021-04-15 14:10:24 -0500

    Make "is this click or tap in a modal?" functionality work on iOS

---
+++ packages/web/utils/dom.ts
@@ -0,0 +1,25 @@
+/* Takes an event and a predicate. Traverses up the DOM evaluating the
+ * prediacte on every ancestor element of the event target, whatever "target"
+ * means in the context of the browser specific event. Use and modify with
+ * caution, the type system is of little help here.
+ */
+const findEventTargetParent = (
+  event: any,
+  pred: (el: HTMLElement) => boolean
+) => {
+  if (event.path) {
+    return event.path.find(pred)
+  } else if (event.srcElement || event.target) {
+    let currentEl: HTMLElement = (event.srcElement || event.target)
+    while (currentEl) {
+      if (pred(currentEl))
+        return currentEl
+
+      currentEl = currentEl.parentElement
+    }
+  }
+}
+
+export {
+  findEventTargetParent
+}

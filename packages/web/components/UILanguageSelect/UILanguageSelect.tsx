390334068996d85c04e7e64d1bc2c45874ed1a06
Ryan Lanny Jenkins <lan.rogers.book@gmail.com>
2021-05-03 21:41:48 -0500

    Make UI language select look like an actual part of the page.

--- packages/web/components/UILanguageSelect/UILanguageSelect.tsx
+++ packages/web/components/UILanguageSelect/UILanguageSelect.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { I18nContext } from 'react-i18next'
-import { i18n } from '@/config/i18n'
+import { useTranslation, i18n } from '@/config/i18n'
 
 import Select from '@/components/Select'
 
@@ -11,11 +11,13 @@ const uiLanguageOptions = [
 ]
 
 const UILanguageSelect = () => {
+  const { t } = useTranslation('settings')
   const {
     i18n: { language },
   } = React.useContext(I18nContext)
   return (
     <Select
+      placeholder={t('profile.uiLanguage.placeholder')}
       options={uiLanguageOptions}
       value={language}
       onChange={(value) => i18n.changeLanguage(value)}

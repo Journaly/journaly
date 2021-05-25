d42828516a7c63b1ed56212c18a277994bab576f
Robin MacPherson <hello@robin-macpherson.co.uk>
2021-05-25 17:15:11 +0100

    Backfill p1

--- packages/j-db-client/prisma/migrations/20210525114631_payments_related_improvements/migration.sql
+++ packages/j-db-client/prisma/migrations/20210525114631_payments_related_improvements/migration.sql
@@ -6,10 +6,17 @@
 
 */
 -- AlterTable
+ALTER TABLE "User" ADD COLUMN     "lastFourCardNumbers" TEXT,
+ADD COLUMN     "cardBrand" TEXT;
+
+-- Backfill Payment Info
+UPDATE "User" SET "lastFourCardNumbers"
+
+-- AlterTable
 ALTER TABLE "MembershipSubscription" DROP COLUMN "lastFourCardNumbers",
 DROP COLUMN "cardBrand",
 ADD COLUMN     "nextBillingDate" TIMESTAMP(3);
 
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "lastFourCardNumbers" TEXT,
-ADD COLUMN     "cardBrand" TEXT;
+-- Backfill nextBillingDate
+UPDATE "MembershipSubscription" SET "nextBillingDate" = "expiresAt";
+

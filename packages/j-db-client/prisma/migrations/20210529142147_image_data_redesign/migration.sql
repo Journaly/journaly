9097bac9566abc1b5ea5169be00accc4c3b2ddc8
Robin MacPherson <robin@kumabrand.com>
2021-05-29 17:43:51 +0100

    First pass at full mig

--- packages/j-db-client/prisma/migrations/20210529142147_image_data_redesign/migration.sql
+++ packages/j-db-client/prisma/migrations/20210529142147_image_data_redesign/migration.sql
@@ -6,17 +6,9 @@
   - Added the required column `headlineImageId` to the `Post` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropForeignKey
-ALTER TABLE "Image" DROP CONSTRAINT "Image_postId_fkey";
 
 -- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "headlineImageId" INTEGER NOT NULL;
-
--- DropTable
-DROP TABLE "Image";
-
--- DropEnum
-DROP TYPE "ImageRole";
+ALTER TABLE "Post" ADD COLUMN     "headlineImageId" INTEGER;
 
 -- CreateTable
 CREATE TABLE "HeadlineImage" (
@@ -27,8 +19,67 @@ CREATE TABLE "HeadlineImage" (
     PRIMARY KEY ("id")
 );
 
+-- Backfill all existing images to new HeadlineImage table
+ALTER TABLE "HeadlineImage" ADD COLUMN "tempPostId" INTEGER;
+
+INSERT INTO "HeadlineImage" (
+  "smallSize",
+  "largeSize",
+  "tempPostId"
+)(
+  SELECT "smallSize", "largeSize", "postId"
+  FROM "Image"
+  WHERE "postId" IS NOT NULL
+);
+
+WITH sq AS (
+  SELECT *
+  FROM "HeadlineImage"
+)
+UPDATE "Post"
+SET "headlineImageId" = sq."id"
+FROM sq
+WHERE "Post".id = sq."tempPostId";
+
+-- Backfill posts that previously used default image
+
+INSERT INTO "HeadlineImage" (
+  "smallSize",
+  "largeSize",
+  "tempPostId"
+)
+SELECT
+  'https://d2ieewwzq5w1x7.cloudfront.net/post-image/f24ad1f4-c934-4e5b-b183-19358856e2ce-small' AS "smallSize",
+  'https://d2ieewwzq5w1x7.cloudfront.net/post-image/f24ad1f4-c934-4e5b-b183-19358856e2ce-large' AS "largeSize",
+  "id" AS "tempPostId"
+FROM "Post"
+WHERE "Post"."headlineImageId" IS NULL;
+
+WITH sq AS (
+  SELECT *
+  FROM "HeadlineImage"
+)
+UPDATE "Post"
+SET "headlineImageId" = sq."id"
+FROM sq
+WHERE "Post".id = sq."tempPostId";
+
+ALTER TABLE "HeadlineImage" DROP COLUMN "tempPostId";
+
+-- Add Null Constraint
+ALTER TABLE "Post" ALTER COLUMN     "headlineImageId" SET NOT NULL;
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Post.headlineImageId_unique" ON "Post"("headlineImageId");
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD FOREIGN KEY ("headlineImageId") REFERENCES "HeadlineImage"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- DropForeignKey
+ALTER TABLE "Image" DROP CONSTRAINT "Image_postId_fkey";
+
+-- DropTable
+DROP TABLE "Image";
+
+-- DropEnum
+DROP TYPE "ImageRole";

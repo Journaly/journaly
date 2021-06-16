fa7849c5980b5925078b423c18d7a448c11a13bf
Robin MacPherson <hello@robin-macpherson.co.uk>
2021-06-16 17:33:42 +0100

    Add logging

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -7,6 +7,8 @@ const dotenvPlugin = require('cypress-dotenv')
 module.exports = (on, config) => {
   config = dotenvPlugin(config)
   require('@cypress/code-coverage/task')(on, config)
+  // Sends all console logs that occur in the browser to stdout in the terminal
+  require('cypress-log-to-output').install(on)
 
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config

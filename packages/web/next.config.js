690c5baf076792b7e88b4d91714f863a549012d7
Robin MacPherson <hello@robin-macpherson.co.uk>
2021-08-12 14:39:57 +0100

    Circle Experiment

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -9,8 +9,8 @@ let config = {
   },
 }
 
-if (process.env.ANALYZE === '1') {
-  config = require('@next/bundle-analyzer')({ enabled: true })(config)
-}
+// if (process.env.ANALYZE === '1') {
+//   config = require('@next/bundle-analyzer')({ enabled: true })(config)
+// }
 
 module.exports = config

816affbc7ccff04fcdb2387dea612910796deb72
Robin MacPherson <hello@robin-macpherson.co.uk>
2021-08-12 16:26:30 +0100

    A wee bit of documentation

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -2,6 +2,8 @@ let config = {
   target: 'serverless',
   productionBrowserSourceMaps: true,
   webpack: (config, { isServer }) => {
+    // This is a workaround due to: https://github.com/prisma/prisma/issues/6564
+    // Should be able to remove this when: https://github.com/prisma/prisma/pull/7931 is merged
     if (isServer) {
       config.externals.push('_http_common', 'bufferutil', 'utf-8-validate')
     }
@@ -9,8 +11,8 @@ let config = {
   },
 }
 
-// if (process.env.ANALYZE === '1') {
-//   config = require('@next/bundle-analyzer')({ enabled: true })(config)
-// }
+if (process.env.ANALYZE === '1') {
+  config = require('@next/bundle-analyzer')({ enabled: true })(config)
+}
 
 module.exports = config

407429da2655f88fe1eea5c38472e5375720c420
Ryan Lanny Jenkins <lan.rogers.book@gmail.com>
2021-09-12 09:26:04 -0500

    Reminder on how to log sql from prisma

--- packages/web/resolvers/utils/db.ts
+++ packages/web/resolvers/utils/db.ts
@@ -10,20 +10,26 @@ declare global {
   }
 }
 
+// Set to true to log from prisma aggressively. Requires server restart.
+const SQL_DEBUG = false
+const PRISMA_OPTIONS = SQL_DEBUG
+  ? { log: ['query', 'info', 'warn', 'error'] }
+  : {}
+
 let _prisma: PrismaClient | null
 
 const getClient = (): PrismaClient => {
   if (process.env.NODE_ENV === 'development') {
     if (!global.prisma) {
       console.log('Creating new database client [DEV]')
-      global.prisma = new PrismaClient()
+      global.prisma = new PrismaClient(PRISMA_OPTIONS)
     }
 
     return global.prisma
   } else {
     if (!_prisma) {
       console.log('Creating new database client [PROD]')
-      _prisma = new PrismaClient()
+      _prisma = new PrismaClient(PRISMA_OPTIONS)
     }
 
     return _prisma

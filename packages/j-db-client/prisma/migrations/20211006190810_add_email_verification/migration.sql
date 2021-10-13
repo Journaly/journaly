ecebbab0ce3b491b221b2669cc525a80fdec377e
Robin MacPherson <hello@robinmacpherson.co>
2021-10-13 08:08:42 +0100

    Write backfill

--- packages/j-db-client/prisma/migrations/20211006190810_add_email_verification/migration.sql
+++ packages/j-db-client/prisma/migrations/20211006190810_add_email_verification/migration.sql
@@ -4,3 +4,7 @@ CREATE TYPE "EmailVerificationStatus" AS ENUM ('PENDING', 'VERIFIED');
 -- AlterTable
 ALTER TABLE "Auth" ADD COLUMN     "emailVerificationToken" TEXT,
 ADD COLUMN     "emailVerificationStatus" "EmailVerificationStatus" NOT NULL DEFAULT E'PENDING';
+
+-- Backfill all existing users as Verified
+UPDATE "Auth"
+SET "emailVerificationStatus" = 'VERIFIED';

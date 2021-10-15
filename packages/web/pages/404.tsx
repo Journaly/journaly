66fe1ddfe8b415e8b66177c65851242425989156
Robin MacPherson <hello@robinmacpherson.co>
2021-10-15 22:15:42 +0100

    Fix build and make the page look a bit nicer

--- packages/web/pages/404.tsx
+++ packages/web/pages/404.tsx
@@ -1,24 +1,41 @@
 import React from 'react'
-import { useTranslation } from '@/config/i18n'
-import { withApollo } from '@/lib/apollo'
+import theme from '@/theme'
+
+// I wrote translation entries for this file but it results in
+// warnings since we cannot use getInitialProps on the 404 page,
+// we cannot requireNamespaces. Left translations in `common.js` for now.
 
 const FourOhFourPage = () => {
-  const { t } = useTranslation('common')
-
   return (
-    <>
-      <div>{t('404')}</div>
-      <style global jsx>{`
+    <div className="container">
+      <h1>404</h1>
+      <p>Oops! The Page you are looking for could not be found</p>
+      <style jsx>{`
+        .container {
+          max-width: 900px;
+          margin: 30vh auto;
+          padding: 50px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          background: ${theme.colors.white};
+          box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
+        }
+
         h1 {
-          line-height: 1;
+          text-align: center;
+          font-weight: 700;
+          font-size: 28px;
+          margin-bottom: 20px;
+        }
+
+        p {
+          margin-bottom: 35px;
         }
       `}</style>
-    </>
+    </div>
   )
 }
 
-FourOhFourPage.getInitialProps = async () => ({
-  namespacesRequired: ['common'],
-})
-
-export default withApollo(FourOhFourPage)
+export default FourOhFourPage

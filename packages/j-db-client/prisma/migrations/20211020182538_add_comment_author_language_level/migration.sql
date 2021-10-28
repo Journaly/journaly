cac58f1584274a4e5b2f91ff42ac97c3553d776d
Robin MacPherson <hello@robinmacpherson.co>
2021-10-28 22:45:12 +0100

    First try

--- packages/j-db-client/prisma/migrations/20211020182538_add_comment_author_language_level/migration.sql
+++ packages/j-db-client/prisma/migrations/20211020182538_add_comment_author_language_level/migration.sql
@@ -7,6 +7,9 @@ ALTER TABLE "PostComment" ADD COLUMN     "authorLanguageLevel" "LanguageLevel" N
 -- Backfill
 -- Update all previous Comments & PostComments to have author's current level
 -- NOTE: comments from authors without the language listed will remain BEGINNER
+
+-- Part 1: Comments
+
 WITH sq AS (
 	SELECT "Comment".id,
 		   "Comment"."authorLanguageLevel",
@@ -14,10 +17,32 @@ WITH sq AS (
 		   "LanguageRelation"."userId",
 		   "LanguageRelation"."level"
 	FROM "Comment"
+  JOIN "Thread" ON "Thread".id = "Comment"."threadId"
+  JOIN "Post" ON "Post".id = "Thread"."postId"
 	JOIN "LanguageRelation"
 	  ON "Comment"."authorId" = "LanguageRelation"."userId"
+    AND "LanguageRelation"."languageId" = "Post"."languageId"
 )
 UPDATE "Comment"
 SET "authorLanguageLevel" = sq.level
 FROM sq
 WHERE "Comment".id = sq.id;
+
+-- Part 2: PostComments
+
+WITH sq AS (
+	SELECT "PostComment".id,
+		   "PostComment"."authorLanguageLevel",
+		   "PostComment"."authorId",
+		   "LanguageRelation"."userId",
+		   "LanguageRelation"."level"
+	FROM "PostComment"
+	JOIN "Post" ON "Post".id = "PostComment"."postId"
+	JOIN "LanguageRelation"
+	  ON "PostComment"."authorId" = "LanguageRelation"."userId"
+    AND "LanguageRelation"."languageId" = "Post"."languageId"
+)
+UPDATE "PostComment"
+SET "authorLanguageLevel" = sq.level
+FROM sq
+WHERE "PostComment".id = sq.id;

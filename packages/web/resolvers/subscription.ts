20f93d58355053d576e1896acad3af78a6d4c275
Robin MacPherson <hello@robinmacpherson.co>
2021-11-24 09:21:11 +0000

    Type-checker happiness

--- packages/web/resolvers/subscription.ts
+++ packages/web/resolvers/subscription.ts
@@ -336,6 +336,9 @@ const MembershipSubscriptionMutations = extendType({
         if (!user?.membershipSubscription?.stripeSubscriptionId) {
           throw new Error("User has no subscription to update")
         }
+        if (!user?.stripeCustomerId) {
+          throw new Error('User has no stripeCustomerId')
+        }
 
         await setPaymentMethod(
           userId,

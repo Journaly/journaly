3d1bd5d92a9a9e3056ba254dbe82a19530dc45c6
Robin MacPherson <hello@robinmacpherson.co>
2021-12-29 09:17:23 +0000

    Add all new BadgeTypes to switch case

--- packages/web/components/Badge/Badge.tsx
+++ packages/web/components/Badge/Badge.tsx
@@ -14,20 +14,80 @@ const assertUnreachable = (x: never): never => {
 
 const getBadgeCopySubpath = (badgeType: BadgeType): string => {
   switch (badgeType) {
+    // Misc Achievements
     case BadgeType.AlphaUser:
       return 'badge.ALPHA_USER'
     case BadgeType.BetaUser:
       return 'badge.BETA_USER'
-    case BadgeType.TenPosts:
-      return 'badge.TEN_POSTS'
-    case BadgeType.OnehundredPosts:
-      return 'badge.ONEHUNDRED_POSTS'
+    case BadgeType.BugHunter:
+      return 'badge.BUG_HUNTER'
     case BadgeType.CodeContributor:
       return 'badge.CODE_CONTRIBUTOR'
-    case BadgeType.Necromancer:
-      return 'badge.NECROMANCER'
     case BadgeType.Odradek:
       return 'badge.ODRADEK'
+    case BadgeType.Necromancer:
+      return 'badge.NECROMANCER'
+    // Posts
+    case BadgeType.TenPosts:
+      return 'badge.TEN_POSTS'
+    case BadgeType.TwentyPosts:
+      return 'badge.TWENTY_POSTS'
+    case BadgeType.FiftyPosts:
+      return 'badge.FIFTY_POSTS'
+    case BadgeType.OnehundredComments:
+      return 'badge.ONEHUNDRED_POSTS'
+    case BadgeType.OnehundredfiftyPosts:
+      return 'badge.ONEHUNDREDFIFTY_POSTS'
+    case BadgeType.TwohundredPosts:
+      return 'badge.TWOHUNDRED_POSTS'
+    case BadgeType.TwohundredfiftyComments:
+      return 'badge.TWOHUNDREDFIFTY_POSTS'
+    // Feedback - Comments
+    case BadgeType.TenComments:
+      return 'badge.TEN_COMMENTS'
+    case BadgeType.FiftyComments:
+      return 'badge.FIFTY_COMMENTS'
+    case BadgeType.OnehundredComments:
+      return 'badge.ONEHUNDRED_COMMENTS'
+    case BadgeType.TwohundredfiftyComments:
+      return 'badge.TWOHUNDREDFIFTY_COMMENTS'
+    case BadgeType.FivehundredComments:
+      return 'badge.FIVEHUNDRED_COMMENTS'
+    case BadgeType.OnethousandComments:
+      return 'badge.ONETHOUSAND_COMMENTS'
+    // Feedback - Posts
+    case BadgeType.CorrectTenPosts:
+      return 'badge.CORRECT_TEN_POSTS'
+    case BadgeType.CorrectTenPosts:
+      return 'badge.CORRECT_TWENTYFIVE_POSTS'
+    case BadgeType.CorrectTenPosts:
+      return 'badge.CORRECT_FIFTY_POSTS'
+    case BadgeType.CorrectTenPosts:
+      return 'badge.CORRECT_ONEHUNDRED_POSTS'
+    case BadgeType.CorrectTenPosts:
+      return 'badge.CORRECT_ONEHUNDREDFIFTY_POSTS'
+    case BadgeType.CorrectTenPosts:
+      return 'badge.CORRECT_TWOHUNDREDFIFTY_POSTS'
+    // Thanks
+    case BadgeType.TenThanks:
+      return 'badge.TEN_THANKS'
+    case BadgeType.FiftyThanks:
+      return 'badge.FIFTY_THANKS'
+    case BadgeType.OnehundredThanks:
+      return 'badge.ONEHUNDRED_THANKS'
+    case BadgeType.TwohundredfiftyThanks:
+      return 'badge.TWOHUNDREDFIFTY_THANKS'
+    case BadgeType.FivehundredThanks:
+      return 'badge.FIVEHUNDRED_THANKS'
+    case BadgeType.OnethousandThanks:
+      return 'badge.ONETHOUSAND_THANKS'
+    case BadgeType.OnethousandtwohundredfiftyThanks:
+      return 'badge.ONETHOUSANDTWOHUNDREDFIFTY_THANKS'
+    case BadgeType.OnethousandfivehundredThanks:
+      return 'badge.ONETHOUSANDFIVEHUNDRED_THANKS'
+    // Goals
+    case BadgeType.Hiker:
+      return 'badge.HIKER'
   }
 
   return assertUnreachable(badgeType)

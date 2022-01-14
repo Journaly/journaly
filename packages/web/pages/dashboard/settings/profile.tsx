420702f95faccb65058316e2429e40ca4be41b2b
Robin MacPherson <hello@robinmacpherson.co>
2022-01-14 17:09:24 +0000

    Remove deprecated pages

--- packages/web/pages/dashboard/settings/profile.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * This page is part of the deprecated URL pattern: `/dashboard/*` and should not be used.
- * See the `redirects` config property in `next.config.js` for all redirect paths.ss
- */
-
-export default function () {
-  return null
-}

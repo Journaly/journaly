b48960d0593eefc2d1af539935d5c7e88e006d73
Robin MacPherson <hello@robinmacpherson.co>
2022-05-12 08:57:53 -0700

    Refactor useAutosavedState to have optional opts

--- packages/web/hooks/useAutosavedState.ts
+++ packages/web/hooks/useAutosavedState.ts
@@ -1,15 +1,22 @@
 import React from 'react'
 
+type UseAutosavedStateOpts = {
+  key: string
+  debounceTime?: number
+  initialTimestamp?: number
+}
+
 export default function useAutosavedState<T>(
   initialValue: T,
-  opts = {
-    key: 'default',
+  opts: UseAutosavedStateOpts,
+): [T, (value: T) => void, () => void] {
+  const mergedOpts = {
     debounceTime: 1000,
     initialTimestamp: 0,
-  },
-): [T, (value: T) => void, () => void] {
+    ...opts,
+  }
   const storage: any = (typeof window !== 'undefined' && window.localStorage) || {}
-  const storageKey = `autosave-v1[${opts.key || 'default'}]`
+  const storageKey = `autosave-v1[${mergedOpts.key || 'default'}]`
 
   const [value, setValue] = React.useState<T>(initialValue)
   const valueRef = React.useRef({ savePending: false, value })
@@ -18,7 +25,7 @@ export default function useAutosavedState<T>(
     if (storageKey in storage) {
       const { value, timestamp } = JSON.parse(storage[storageKey])
 
-      if (timestamp > opts.initialTimestamp) {
+      if (timestamp > mergedOpts.initialTimestamp) {
         console.info('Restoring value from storage')
         setValue(value)
       }
@@ -38,7 +45,7 @@ export default function useAutosavedState<T>(
         })
         valueRef.current.savePending = false
         console.info(`Saved value on key ${storageKey}`)
-      }, opts.debounceTime)
+      }, mergedOpts.debounceTime)
     }
   }, [value])
 

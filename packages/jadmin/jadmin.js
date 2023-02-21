f6873fd3d0096c755bad928872d574aeb4dec089
Robin MacPherson <hello@robinmacpherson.co>
2023-02-21 10:13:50 -0800

    Factor out `deletePosts` fn and get everything working

--- packages/jadmin/jadmin.js
+++ packages/jadmin/jadmin.js
@@ -4,6 +4,54 @@ const { Pool } = require('pg')
 const pgTag = require('pg-tag')
 const yargs = require('yargs')
 
+async function deletePosts(postIds, query) {
+  if (!postIds.length) return
+
+  await query`
+    DELETE
+    FROM "PostClap"
+    WHERE "postId" IN (${postIds})
+  `
+
+  await query`
+    DELETE
+    FROM "PostCommentSubscription"
+    WHERE "postId" IN (${postIds})
+  `
+
+  await query`
+    DELETE
+    FROM "PostTopic"
+    WHERE "postId" IN (${postIds})
+  `
+
+  await query`
+    DELETE
+    FROM "Comment"
+    USING "Thread"
+    WHERE "Comment"."threadId" = "Thread"."id"
+      AND "postId" IN (${postIds})
+  `
+
+  await query`
+    DELETE
+    FROM "Thread"
+    WHERE "postId" IN (${postIds})
+  `
+
+  await query`
+    DELETE
+    FROM "PostComment"
+    WHERE "postId" IN (${postIds})
+  `
+
+  await query`
+        DELETE
+        FROM "Post"
+        WHERE "id" IN (${postIds})
+      `
+}
+
 yargs
   .scriptName('jadmin')
   .command({
@@ -25,6 +73,21 @@ yargs
       `
 
       const query = db.transaction()
+      // const query = db.query
+
+      const postIds = (
+        await db.all`
+        SELECT id
+        FROM "Post"
+        WHERE "authorId" = ${userId}
+      `
+      ).map((post) => post.id)
+
+      console.log(postIds)
+
+      await deletePosts(postIds, query)
+
+      console.log('test')
 
       await query`
         DELETE
@@ -32,11 +95,14 @@ yargs
         WHERE "userId" = ${userId}
       `
 
+      console.log('test2')
+
       await query`
         DELETE
         FROM "PostClap"
         WHERE "authorId" = ${userId}
       `
+      console.log('test3')
 
       // We need to handle claps left by others on the post that is being deleted
       await query`
@@ -44,70 +110,82 @@ yargs
         FROM "Post"
         WHERE "authorId" = ${userId}
       `
+      console.log('test PostComment')
 
       await query`
         DELETE
         FROM "PostComment"
         WHERE "authorId" = ${userId}
       `
+      console.log('test PostCommentSubscription')
 
       await query`
         DELETE
         FROM "PostCommentSubscription"
         WHERE "userId" = ${userId}
       `
+      console.log('test UserBadge')
 
       await query`
         DELETE
         FROM "UserBadge"
         WHERE "userId" = ${userId}
       `
+      console.log('test PendingNotification')
 
       await query`
         DELETE
         FROM "PendingNotification"
         WHERE "userId" = ${userId}
       `
+      console.log('test LanguageRelation')
 
       await query`
         DELETE
         FROM "LanguageRelation"
         WHERE "userId" = ${userId}
       `
+      console.log('test InAppNotification')
 
       await query`
         DELETE
         FROM "InAppNotification"
         WHERE "userId" = ${userId} OR "triggeringUserId" = ${userId}
       `
+      console.log('test Comment')
 
       await query`
         DELETE
         FROM "Comment"
         WHERE "authorId" = ${userId}
       `
+      console.log('test CommentThanks')
 
       await query`
         DELETE
         FROM "CommentThanks"
         WHERE "authorId" = ${userId}
       `
+      console.log('test Auth')
 
       await query`
         DELETE
         FROM "Auth"
         WHERE "userId" = ${userId}
       `
+      console.log('test User')
 
       await query`
         DELETE
         FROM "User"
         WHERE "id" = ${userId}
       `
+      console.log('test user')
 
       await query.commit()
 
       console.table(user)
+      await db.pool.end()
     },
   })
   .demandCommand(1)

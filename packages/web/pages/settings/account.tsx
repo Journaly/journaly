639d77e6ed7512ae4dca694d1f2d806ad2f23d35
Robin MacPherson <hello@robinmacpherson.co>
2023-04-08 10:16:15 -0700

    Add new NotificationSettingsForm to account settings

--- packages/web/pages/settings/account.tsx
+++ packages/web/pages/settings/account.tsx
@@ -4,23 +4,35 @@ import { withApollo } from '@/lib/apollo'
 import SettingsPageLayout from '@/components/Layouts/SettingsPageLayout'
 import AuthGate from '@/components/AuthGate'
 import UpdatePasswordForm from '@/components/Dashboard/Settings/UpdatePasswordForm'
+import NotificationSettingsForm from '@/components/Dashboard/Settings/NotificationSettingsForm'
 
 const Account: NextPage = () => {
   return (
     <AuthGate>
-      <>
-        <SettingsPageLayout>
+      {(currentUser) => {
+        return (
+          <SettingsPageLayout>
           <div className="forms-container">
             <UpdatePasswordForm />
+            <NotificationSettingsForm userConfiguration={currentUser.configuration} />
           </div>
+          <style jsx>{`
+            .forms-container {
+              width: 100%;
+              max-width: 1008px;
+            }
+
+            .forms-container :global(form) {
+              margin-bottom: 40px;
+            }
+
+            .forms-container :global(form):last-child {
+              margin-bottom: 0;
+            }
+          `}</style>
         </SettingsPageLayout>
-        <style jsx>{`
-          .forms-container {
-            width: 100%;
-            max-width: 1008px;
-          }
-        `}</style>
-      </>
+        )
+      }}
     </AuthGate>
   )
 }

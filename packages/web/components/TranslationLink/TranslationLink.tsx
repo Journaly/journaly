ad67c394251cfe74af080efd32fd33c0428da8a1
Robin MacPherson <hello@robinmacpherson.co>
2023-05-04 08:45:23 -0700

    Add explicit `children` prop typing

--- packages/web/components/TranslationLink/TranslationLink.tsx
+++ packages/web/components/TranslationLink/TranslationLink.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Link, { LinkProps } from 'next/link'
 
-const TranslationLink: React.FC<LinkProps> = ({ href, children, ...props }) => {
+type TranslationLinkProps = LinkProps & {
+  children: React.ReactNode
+}
+
+const TranslationLink: React.FC<TranslationLinkProps> = ({ href, children, ...props }) => {
   return (
     <Link href={href} {...props} legacyBehavior>
       <a className="j-link">{children}</a>

55f0944c5e991220af4e582cac38f121425962e8
Robin MacPherson <hello@robinmacpherson.co>
2023-10-30 15:59:19 -0700

    Start processing oldStr & newStr for rendering

--- packages/web/utils/rehypeSuggestions.ts
+++ packages/web/utils/rehypeSuggestions.ts
@@ -24,25 +24,52 @@ export default function rehypedSuggestions(options: any) {
 
       node.properties.className = ['suggestion']
 
-      const oldStrDiv = document.createElement('div')
-      const newStrDiv = document.createElement('div')
+      const textNode = node.children[0].children[0]
+      const originalStr = options.baseContent
+      const suggestionStr = textNode.value
 
-      oldStrDiv.textContent = 'old'
-      newStrDiv.textContent = 'new'
+      const { lcs, oldStr, newStr } = suggestionDiff(originalStr, suggestionStr)
+      // console.log('diff', diff)
+
+      const oldStrDiv = document.createElement('div')
+      oldStrDiv.classList.add('old-string')
+      const newStrDiv = document.createElement('div')
+      newStrDiv.classList.add('new-string')
+
+      for (const el of oldStr) {
+        const newEl = oldStrDiv.appendChild(document.createElement('span'))
+        newEl.textContent = el[1]
+        // console.log(el)
+        if (el[0] === ' ') {
+          continue
+        } else if (el[0] === '-') {
+          newEl.classList.add('del')
+        } else if (el[0] === '+') {
+          newEl.classList.add('add')
+        }
+      }
+
+      for (const el of newStr) {
+        const newEl = newStrDiv.appendChild(document.createElement('span'))
+        newEl.textContent = el[1]
+
+        if (el[0] === ' ') {
+          continue
+        } else if (el[0] === '-') {
+          newEl.classList.add('del')
+        } else if (el[0] === '+') {
+          newEl.classList.add('add')
+        }
+      }
+
+      // oldStrDiv.textContent = 'old'
+      // newStrDiv.textContent = 'new'
 
       console.log(oldStrDiv)
       console.log(newStrDiv)
 
       // node.appendChild(oldStrDiv)
       // node.appendChild(newStrDiv)
-      console.log('node', node)
-      const textNode = node.children[0].children[0]
-      console.log('textNode', textNode)
-      const originalStr = options.baseContent
-      const suggestionStr = textNode.value
-
-      const diff = suggestionDiff(originalStr, suggestionStr)
-      console.log('diff', diff)
 
       textNode.value = `- ${options.baseContent} \n + ${textNode.value}`
 
@@ -52,7 +79,7 @@ export default function rehypedSuggestions(options: any) {
       data.hChildren = 'foobar'
 
       props.className = ['suggestion']
-      console.log('node', node)
+      // console.log('node', node)
     })
   }
 }

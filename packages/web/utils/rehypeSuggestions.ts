bcf1eae66b76212de2592adcc505b6592ac493bf
Robin MacPherson <hello@robinmacpherson.co>
2023-11-02 18:53:29 -0700

    Get oldStr and suggestedStr rendering!

--- packages/web/utils/rehypeSuggestions.ts
+++ packages/web/utils/rehypeSuggestions.ts
@@ -23,43 +23,88 @@ export default function rehypedSuggestions(options: any) {
       }
 
       node.properties.className = ['suggestion']
-
+      console.log(node)
       const textNode = node.children[0].children[0]
       const originalStr = options.baseContent
       const suggestionStr = textNode.value
 
       const { lcs, oldStr, newStr } = suggestionDiff(originalStr, suggestionStr)
+      console.log('lcs', lcs)
       // console.log('diff', diff)
 
-      const oldStrDiv = document.createElement('div')
-      oldStrDiv.classList.add('old-string')
-      const newStrDiv = document.createElement('div')
-      newStrDiv.classList.add('new-string')
-
-      for (const el of oldStr) {
-        const newEl = oldStrDiv.appendChild(document.createElement('span'))
-        newEl.textContent = el[1]
-        // console.log(el)
-        if (el[0] === ' ') {
-          continue
-        } else if (el[0] === '-') {
-          newEl.classList.add('del')
-        } else if (el[0] === '+') {
-          newEl.classList.add('add')
-        }
+      // const oldStrDiv = document.createElement('div')
+      const oldStrDiv = {
+        type: 'element',
+        tagName: 'div',
+        // TODO: Fix type
+        children: [] as any[],
+        properties: {
+          className: ['old-string'],
+        },
       }
 
-      for (const el of newStr) {
-        const newEl = newStrDiv.appendChild(document.createElement('span'))
-        newEl.textContent = el[1]
+      const newStrDiv = {
+        type: 'element',
+        tagName: 'div',
+        // TODO: Fix type
+        children: [] as any[],
+        properties: {
+          className: ['new-string'],
+        },
+      }
 
-        if (el[0] === ' ') {
-          continue
-        } else if (el[0] === '-') {
-          newEl.classList.add('del')
-        } else if (el[0] === '+') {
-          newEl.classList.add('add')
+      console.log('oldStr', oldStr)
+      console.log('newStr', newStr)
+      // oldStrDiv.classList.add('old-string')
+      // const newStrDiv = document.createElement('div')
+      // newStrDiv.classList.add('new-string')
+
+      for (const [mode, value] of oldStr) {
+        const newEl = {
+          type: 'element',
+          tagName: 'span',
+          children: [
+            {
+              type: 'text',
+              value: value,
+            },
+          ],
+          properties: {
+            className: [] as string[],
+          },
         }
+
+        // console.log(el)
+        if (mode === '-') {
+          newEl.properties.className.push('del')
+        } else if (mode === '+') {
+          newEl.properties.className.push('add')
+        }
+
+        oldStrDiv.children.push(newEl)
+      }
+
+      for (const [mode, value] of newStr) {
+        const newEl = {
+          type: 'element',
+          tagName: 'span',
+          children: [
+            {
+              type: 'text',
+              value: value,
+            },
+          ],
+          properties: {
+            className: [] as string[],
+          },
+        }
+
+        if (mode === '-') {
+          newEl.properties.className.push('del')
+        } else if (mode === '+') {
+          newEl.properties.className.push('add')
+        }
+        newStrDiv.children.push(newEl)
       }
 
       // oldStrDiv.textContent = 'old'
@@ -72,6 +117,7 @@ export default function rehypedSuggestions(options: any) {
       // node.appendChild(newStrDiv)
 
       textNode.value = `- ${options.baseContent} \n + ${textNode.value}`
+      node.children[0].children = [oldStrDiv, newStrDiv]
 
       const props: Properties = data.hProperties || (data.hProperties = {})
 

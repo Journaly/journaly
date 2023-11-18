4f8536273efff88bf21bbb12cba112e981a03666
Robin MacPherson <hello@robinmacpherson.co>
2023-11-18 09:39:04 -0800

    Configure Jest

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
-  preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
   testEnvironment: 'node',
-};
+}

4f0eaabe94b9291349893fe962d3a773adc4f304
Robin MacPherson <hello@robinmacpherson.co>
2024-05-30 06:41:27 -0700

    Wrap app root in ApolloProvider

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import App from 'next/app'
 import Head from 'next/head'
 import Router, { withRouter } from 'next/router'
@@ -12,20 +13,22 @@ import '@/styles/globalStyles.css'
 import 'react-toastify/dist/ReactToastify.css'
 import 'reactjs-popup/dist/index.css'
 import '@/styles/react-toastify-overrides.css'
+import { ApolloProvider } from '@apollo/client'
+import { initApolloClient } from '@/lib/apollo'
 
 // Show loading progress on page loads
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeError', () => NProgress.done())
 Router.events.on('routeChangeComplete', () => NProgress.done())
-
 class JournalyApp extends App {
   componentDidMount() {}
 
   render() {
     const { Component, pageProps } = this.props
+    const client = initApolloClient(pageProps.apolloState)
 
     return (
-      <>
+      <ApolloProvider client={client}>
         <Head>
           <title>Journaly</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -34,7 +37,7 @@ class JournalyApp extends App {
         <GoogleAnalytics />
         <Component {...pageProps} />
         <ToastContainer />
-      </>
+      </ApolloProvider>
     )
   }
 }

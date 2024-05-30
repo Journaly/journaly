a6561fd6a2cc6c38aee56c14a964d86d213601f3
Robin MacPherson <hello@robinmacpherson.co>
2024-05-30 06:49:57 -0700

    Factor `getUiLanguage` out

---
+++ packages/web/lib/getUiLanguage.ts
@@ -0,0 +1,27 @@
+import { NextPageContext } from 'next'
+import { langCodeToUILangMap } from '@/hooks/useUILanguage'
+import { UiLanguage } from '@/generated/graphql'
+import i18nConfig from '@/config/i18n'
+
+export const getUiLanguage = (ctx: NextPageContext): UiLanguage => {
+  let langCode
+  if (ctx.req) {
+    const i18n = ctx.req.i18n as any
+    if (!i18n) {
+      langCode = 'en'
+    } else {
+      const { allLanguages, defaultLanguage, fallbackLng } = i18n.options
+      const fallback = fallbackLng || defaultLanguage
+
+      if (!i18n.languages) {
+        langCode = typeof fallback === 'string' ? fallback : null
+      } else {
+        langCode = i18n.languages.find((lang: string) => allLanguages.includes(lang)) || fallback
+      }
+    }
+  } else {
+    langCode = i18nConfig.i18n.language
+  }
+
+  return langCodeToUILangMap[langCode] || UiLanguage.English
+}

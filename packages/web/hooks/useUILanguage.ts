662cf6675745a2f90cb35d1dd67699a1e6187702
Robin MacPherson <hello@robinmacpherson.co>
2024-05-30 06:39:53 -0700

    Use all languages in langCodeToUiLanguage

--- packages/web/hooks/useUILanguage.ts
+++ packages/web/hooks/useUILanguage.ts
@@ -3,12 +3,15 @@ import { I18nContext } from 'react-i18next'
 
 import { UiLanguage as UILanguage } from '@/generated/graphql'
 
-const langCodeToUILangMap: { [key: string]: UILanguage } = {
+export const langCodeToUILangMap: { [key: string]: UILanguage } = {
   en: UILanguage.English,
   de: UILanguage.German,
   es: UILanguage.Spanish,
+  zh_CN: UILanguage.ChineseSimplified,
+  zh_TW: UILanguage.ChineseTraditional,
+  it: UILanguage.Italian,
+  pt_BR: UILanguage.PortugueseBrazilian,
 }
-
 
 const useUILanguage = () => {
   const { i18n: { language } } = React.useContext(I18nContext)
